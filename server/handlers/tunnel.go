5076fa213b1a5f2bcee008c57cb4b845e47dbd62
Javokhir Nematov <nematovjavohir58@gmail.com>
2024-04-21 10:36:17 +0500

    Removed client DB connection from this branch

--- server/handlers/tunnel.go
+++ server/handlers/tunnel.go
@@ -14,25 +14,8 @@ import (
 
 	utilities "github.com/globe-and-citizen/layer8-utils"
 )
-
-type ClientPortalRecord struct {
-	ID   int
-	Secret string
-	Name string
-	RedirectURI string
-	Username string
-	Password string
-	Salt string
-}
-
 // Tunnel forwards the request to the service provider's backend
 func InitTunnel(w http.ResponseWriter, r *http.Request) {
-	clientPortalData, err := getClientPortalData()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(clientPortalData)
-
 	fmt.Println("\n\n*************")
 	fmt.Println(r.Method) // > GET  | > POST
 	fmt.Println(r.URL)    // (http://localhost:5000/api/v1 ) > /api/v1
@@ -221,38 +204,4 @@ func TestError(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Test error endpoint:", err.Error())
 	http.Error(w, err.Error(), http.StatusInternalServerError)
 	return
-}
-
-func getClientPortalData() ([]ClientPortalRecord, error) {
-    db, err := sql.Open("postgres", "postgres://root:1234@localhost/ResourceServer?sslmode=disable")
-    if err != nil {
-        return nil, err
-    }
-    defer db.Close()
-
-    query := "SELECT * FROM clients"
-
-    rows, err := db.Query(query)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
-
-    var clientPortalData []ClientPortalRecord
-
-    for rows.Next() {
-        var record ClientPortalRecord
-
-        if err := rows.Scan(&record.ID, &record.Name, &record.Secret, &record.RedirectURI, &record.Username, &record.Password, &record.Salt); err != nil {
-            return nil, err
-        }
-
-        clientPortalData = append(clientPortalData, record)
-    }
-
-    if err := rows.Err(); err != nil {
-        return nil, err
-    }
-
-    return clientPortalData, nil
 }
