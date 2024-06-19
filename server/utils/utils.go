ccb4e05c689ddf355fd3044345dd57ed54cdb859
M. Firhan Azmi Nor <muhammad.firhan.azmi@gmail.com>
2024-06-19 04:11:37 +0000

    refactor: authorization handler to be more testable

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -2,7 +2,12 @@ package utils
 
 import (
 	"crypto/sha1"
+	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
 
 	"github.com/xdg-go/pbkdf2"
 )
@@ -10,4 +15,51 @@ import (
 func SaltAndHashPassword(password string, salt string) string {
 	dk := pbkdf2.Key([]byte(password), []byte(salt), 4096, 32, sha1.New)
 	return hex.EncodeToString(dk[:])
+}
+
+func WriteJSONResponse(
+	w http.ResponseWriter,
+	status int,
+	data interface{},
+) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+type JSONResponseInput struct {
+	StatusCode int
+	Data       interface{}
+}
+
+type RedirectResponseInput struct {
+	StatusCode int
+	Location   string
+}
+
+func MapResponse(
+	isJson bool,
+	w http.ResponseWriter,
+	j *JSONResponseInput,
+	red *RedirectResponseInput,
+) {
+	if isJson {
+		WriteJSONResponse(w, j.StatusCode, j.Data)
+	} else {
+		http.Redirect(w, &http.Request{}, red.Location, red.StatusCode)
+	}
+}
+
+func GetClientIDAndSecretFromAuthHeader(t string) (string, string, error) {
+	t = strings.TrimPrefix(t, "Basic ")
+	b, err := base64.StdEncoding.DecodeString(t)
+	if err != nil {
+		return "", "", err
+	}
+	// first remove the "Basic " prefix
+	s := strings.SplitN(string(b), ":", 2)
+	if len(s) != 2 {
+		return "", "", errors.New("invalid basic auth header")
+	}
+	return s[0], s[1], nil
 }
