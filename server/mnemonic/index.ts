b07a5b8a0f6c588689e51ee977a9540de5bf2ac0
Veronika <58723287+interestIngc@users.noreply.github.com>
2024-10-06 21:13:25 +0300

    resolved issue with imports

--- server/mnemonic/index.ts
+++ server/mnemonic/index.ts
@@ -3,11 +3,11 @@ import { wordlist } from '@scure/bip39/wordlists/english';
 
 import * as bip32 from "@scure/bip32"
 
-export function generateMnemonic(): string {
+export function generateBip39Mnemonic(): string {
     return bip39.generateMnemonic(wordlist, 128)
 }
 
-export function validateMnemonic(mnemonic: string): boolean {
+export function isMnemonicValid(mnemonic: string): boolean {
     return bip39.validateMnemonic(mnemonic, wordlist)
 }
 
@@ -17,24 +17,9 @@ export function getBinarySeed(mnemonic: string): Uint8Array {
 
 export function getPrivateAndPublicKeys(binarySeed: Uint8Array) {
     // generate a master key
-    return bip32.HDKey.fromMasterSeed(binarySeed)
-}
-
-function test() {
-    const mnemonic = generateMnemonic()
-    console.log(mnemonic)
-
-    const isValid = validateMnemonic(mnemonic)
-    console.log(isValid)
-
-    const seed = getBinarySeed(mnemonic)
-    console.log(seed)
-
-    const hdKey = getPrivateAndPublicKeys(seed)
-
-    console.log("Private key:")
-    console.log(hdKey.privateKey)
-
-    console.log("Public key:")
-    console.log(hdKey.publicKey)
+    const hdKey = bip32.HDKey.fromMasterSeed(binarySeed)
+    return {
+        "privateKey": hdKey.privateKey,
+        "publicKey": hdKey.publicKey
+    }
 }
