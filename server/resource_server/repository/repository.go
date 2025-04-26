c008ac64ba6e42ba7acdb950bfd5471a78543089
Veronika <58723287+interestIngc@users.noreply.github.com>
2025-04-26 09:33:52 +0200

    improved pay with crypto UI

--- server/resource_server/repository/repository.go
+++ server/resource_server/repository/repository.go
@@ -450,7 +450,6 @@ func (r *Repository) CreateClientTrafficStatisticsEntry(clientId string, rate in
 }
 
 func (r *Repository) GetClientTrafficStatistics(clientId string) (*models.ClientTrafficStatistics, error) {
-	// TODO: is isolation level higher then the default needed?
 	var clientStatistics models.ClientTrafficStatistics
 
 	err := r.connection.Model(&models.ClientTrafficStatistics{}).
